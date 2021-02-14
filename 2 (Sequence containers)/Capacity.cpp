c54ab5ecb75d000df67f75a39d7a8bf4e71e7d20
Ko-dst-ya <78589527+Ko-dst-ya@users.noreply.github.com>
2021-02-14 17:31:07 +0300

    Update Capacity.cpp

--- 2 (Sequence containers)/Capacity.cpp
+++ 2 (Sequence containers)/Capacity.cpp
@@ -9,27 +9,29 @@ int main()
 
     for (auto i = 0; i <= 31; ++i)
     {
-        vec.push_back(i);
-		
+        vec.push_back(i);	
     }
 
-	double coef = vec.capacity();
+	double coef = vec.capacity();	// Коэффициент расширения
+	
 	std::cout << "Size      = " << vec.size() << std::endl;
 	std::cout << "Capacity  = " << vec.capacity() << std::endl;
 
     std::cout << "\nAfter adding one element:" << std::endl;
 
 	vec.push_back(31);
+	
 	std::cout << "Size      = " << vec.size() << std::endl;
 	std::cout << "Capacity  = " << vec.capacity() << std::endl;
 	
-	coef = (double)(vec.capacity()) / coef;
+	coef = (double)vec.capacity() / coef;
 	std::cout << "\nCoefficient of expansion is " << coef << std::endl; // Коэффициент расширения равен 2 на MinGW
 
     std::cout << "\nUsing the reserve() member function\n" << std::endl;
 
     std::vector < int > vec_2;
 	vec_2.reserve(5);
+	
 	std::cout << "Size      = " << vec_2.size() << std::endl;
 	std::cout << "Capacity  = " << vec_2.capacity() << std::endl; 
     // На MinGW ёмкость равна запрошенному месту
