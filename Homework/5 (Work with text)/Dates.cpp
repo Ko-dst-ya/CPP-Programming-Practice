2b4415442f3952665f12671166e55fd0ce3b913d
Ko-dst-ya <78589527+Ko-dst-ya@users.noreply.github.com>
2021-03-20 22:26:21 +0300

    Update Dates.cpp
    Changed pattern for date and data-string

--- Homework/5 (Work with text)/Dates.cpp
+++ Homework/5 (Work with text)/Dates.cpp
@@ -4,17 +4,16 @@
 
 int main()
 {   
-    //https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy
-    const std::regex pattern
-            (R"(^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$)");
+    const std::regex pattern_for_date(R"(\d{4}\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01]))");
+    
+    std::string data = "kjsag;ajlsd 0094.02.10 itjjgr 30:90:00 ated 2021.01.01 asg 17:00:59 y 9001.01.32 aisn 10:40:34 hgffk 20.0.0 hgf 1598.12.30";
 
-    std::string adf = "JLDJfd sdaj 29-0,-11 12/03/2011 354jj 3512 fdg 02/12/3544 34jg 54/34/2434";
-    std::string data = "01/02/2011";
-
-    std::sregex_iterator begin(data.cbegin(), data.cend(), pattern);
-    std::sregex_iterator end;
-
-    std::for_each(begin, end, [](const std::smatch & m)
+    std::sregex_iterator date_begin(data.cbegin(), data.cend(), pattern_for_date);
+    std::sregex_iterator date_end;
+    
+    std::cout << "Dates:\n";
+    
+    std::for_each(date_begin, date_end, [](const std::smatch & m)
         {
             std::cout << m[0] << std::endl;
         });
