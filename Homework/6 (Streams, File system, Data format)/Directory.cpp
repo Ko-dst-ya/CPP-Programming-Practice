89f0734592e611414a1d0d86b63d5fc31df35585
Ko-dst-ya <78589527+Ko-dst-ya@users.noreply.github.com>
2021-03-21 23:10:28 +0300

    Update Directory.cpp
    Removed unnecessary code

--- Homework/6 (Streams, File system, Data format)/Directory.cpp
+++ Homework/6 (Streams, File system, Data format)/Directory.cpp
@@ -15,44 +15,6 @@ std::time_t to_time_t(TP tp)
     return system_clock::to_time_t(sctp);
 }
 
-auto compute_file_size(const std::filesystem::path & path)
-{
-	if (std::filesystem::exists(path) && std::filesystem::is_regular_file(path))
-	{
-		auto file_size = std::filesystem::file_size(path);
-
-		if (file_size != static_cast <uintmax_t> (-1))
-		{
-			return file_size;
-		}
-	}
-
-	return static_cast <uintmax_t> (-1);
-}
-
-auto compute_directory_size(const std::filesystem::path & path)
-{
-	uintmax_t size = 0ULL;
-
-	if (std::filesystem::exists(path) && std::filesystem::is_directory(path))
-	{
-		for (auto const & entry : std::filesystem::recursive_directory_iterator(path))
-		{
-			if (std::filesystem::is_regular_file(entry.status()) || std::filesystem::is_symlink(entry.status()))
-			{
-				auto file_size = std::filesystem::file_size(entry);
-
-				if (file_size != static_cast <uintmax_t> (-1))
-				{
-					size += file_size;
-				}
-			}
-		}
-	}
-
-	return size;
-}
-
 void view_directory(const std::filesystem::path & path)
 {
 	if (std::filesystem::exists(path) && std::filesystem::is_directory(path))
@@ -71,10 +33,6 @@ void view_directory(const std::filesystem::path & path)
 int main(int argc, char ** argv)
 {
 	system("chcp 1251");
-
-	std::cout << compute_file_size("./Source.cpp") << std::endl;
-
-	std::cout << compute_directory_size(std::filesystem::current_path()) << std::endl;
 
 	view_directory(std::filesystem::current_path());
 
